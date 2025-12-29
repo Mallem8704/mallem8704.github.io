b9f88c47fe65bc528cdce17985380b6d8146c47c
Mallem8704 <mallemdheerajroyal@gmail.com>
2025-12-29 10:02:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ const revealOnScroll = () => {
     }
   });
 };
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll(".achievement-card").forEach(card => {
+  card.style.opacity = "0";
+  card.style.transform = "translateY(40px)";
+  observer.observe(card);
+});
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
